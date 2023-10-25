51c12673fd73ee1ab1b834c5a412d9739d6712a0
MLibralato91 <michelelibralato911@gmail.com>
2023-10-25 17:43:35 +0200

    add navbar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,27 +4,50 @@ import { RouterLink } from 'vue-router';
 
 
 export default {
-    components: {
-        RouterLink,
-    },
+  components: {
+    RouterLink,
+  },
 
-    data() {
-        return {
-           
-        }
-    }}
-    
+  data() {
+    return {
+
+    }
+  }
+}
+
 </script>
 
 <template>
-    <router-link :to="{ name: 'home' }" aria-current="page">Homepage</router-link>
-    <router-link :to="{ name: 'doctor' }" aria-current="page">DoctorDetails</router-link>
-
-        
+  <header>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">B-Doctors</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#"><router-link :to="{ name: 'home' }"
+                  aria-current="page">Home</router-link></a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#"><router-link :to="{ name: 'doctor' }" aria-current="page">I nostri
+                  Dottori</router-link></a>
+            </li>
+          </ul>
+        </div>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Cerca</button>
+        </form>
+      </div>
+    </nav>
+  </header>
 </template>
 
 
 <style scoped lang="scss">
 @use "../styles/general.scss" as *;
-
 </style>
