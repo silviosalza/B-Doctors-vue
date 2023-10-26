633eaf4380ef8d13e4c2cd3db0b9dcdec2525d0b
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-10-26 12:11:45 +0200

    add search for typology

--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -16,16 +16,33 @@ export default {
   data() {
     return {
       doctors: [],
-      myUrl: 'http://localhost:8000',
+      typologies: [],
+      selectedType: [],
+      myUrl: 'http://localhost:8000/api/doctors?',
     }
   },
   methods: {
     getDoctors() {
-      axios.get(`${this.myUrl}/api/doctors`)
+
+      // IF filter
+      let typeUrl = '';
+
+      for (let i = 0; i < this.selectedType.length; i++) {
+
+          // it return the complete API URL for the Axios call, with filter too
+          typeUrl = typeUrl + '&typology_id[]=' + this.selectedType[i];
+
+          console.log(typeUrl);
+
+      };
+
+      axios.get(this.myUrl + typeUrl)
         .then(response => {
           this.doctors = response.data.results;
           this.typologies = response.data.typologies;
           console.log(this.doctors);
+          // console.log(this.typologies);
+          // console.log(this.myUrl + typeUrl);
         })
         .catch(error => {
           console.error(error);
@@ -45,18 +62,22 @@ export default {
   <div class="container">
     <h1>homepage</h1>
     <div class="search">
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Cerca</button>
-      </form>
-      <div class="typologies">
-        <div v-for="(typology, index) in typologies" class="form-check">
-          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
-          <label class="form-check-label" for="flexCheckDefault">
-            {{ typology.name }}
-          </label>
+      <form @submit.prevent="">
+        <ul class="list-group d-flex gap-3">
+            <li class="list-group-item rounded-4 border-top-0 border-start-0 border-end-0 bg-dark text-white"
+                v-for="(typology, index) in typologies" :key="index">
+                <div class="form-check">
+                    <input :value="typology.id" class="form-check-input checkbox-filter" type="checkbox"
+                        id="typology_id" name="typologies_id[]" v-model="selectedType">
+                    <label for="typology_id" class="form-check-label">{{ typology.name }}</label>
+                </div>
+            </li>
+        </ul>
+        <div class="text-center">
+            <button class="btn btn-restaurant mt-4" @click="getDoctors"
+                type="submit">Filtra</button>
         </div>
-      </div>
+      </form>
     </div>
     <div class="row">
       <div class="col-md-3 col-6 col-sm-4 col-xl-3" v-for="(element, index) in doctors" :key="index">
