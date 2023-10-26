6e41a47de3ea478938c8637f82b07c8c1bd6f32d
MLibralato91 <michelelibralato911@gmail.com>
2023-10-26 10:53:09 +0200

    add search bar

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -38,10 +38,7 @@ export default {
             </li>
           </ul>
         </div>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Cerca</button>
-        </form>
+
       </div>
     </nav>
   </header>
--- src/pages/Homepage.vue
+++ src/pages/Homepage.vue
@@ -24,6 +24,7 @@ export default {
       axios.get(`${this.myUrl}/api/doctors`)
         .then(response => {
           this.doctors = response.data.results;
+          this.typologies = response.data.typologies;
           console.log(this.doctors);
         })
         .catch(error => {
@@ -43,6 +44,20 @@ export default {
 <template>
   <div class="container">
     <h1>homepage</h1>
+    <div class="search">
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Cerca" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Cerca</button>
+      </form>
+      <div class="typologies">
+        <div v-for="(typology, index) in typologies" class="form-check">
+          <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+          <label class="form-check-label" for="flexCheckDefault">
+            {{ typology.name }}
+          </label>
+        </div>
+      </div>
+    </div>
     <div class="row">
       <div class="col-md-3 col-6 col-sm-4 col-xl-3" v-for="(element, index) in doctors" :key="index">
         <DoctorCard :doctor="element" />
